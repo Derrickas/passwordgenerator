892519c62476161686bb240db0332d59232c8fe3
Derrickas <Derrickas@outlook.com>
2019-12-18 21:40:11 -0600

    added more variables

--- Javascript.js
+++ Javascript.js
@@ -1,20 +1,37 @@
-const symbols = '~!@#$%^&*()_+{}|:"<>?-=`'
-const lowercase = 'abcdefghijklmnopqrstuvwxyz'
-const uppercase = lowercase.toUpperCase()
+var resultsEL =document.querySelector('symbol-box')
+var lengthEL =document.querySelector('length')
+var uppercasesEL =document.querySelector('uppercase-box')
+var lowercaseEL =document.querySelector('lowercase-box')
+var generateEL =document.querySelector('generate')
+var clipboardEL =document.querySelector('clipboard')
+
+
+
+const symbolsvar = '~!@#$%^&*()_+{}|:"<>?-=`'
+const lowercasevar = 'abcdefghijklmnopqrstuvwxyz'
+const uppercasevar = lowercase.toUpperCase()
+const numbervar = '0123456789'
+
+const randomFunc = {
+    Upper: GetRandomUpper
+    lower: GetRandomLower
+    symbols: GetRandomSymbol
+    number: GetRandomUpper
+};
 
 
 function GetRandomLower(){
- return lowercase [Math.floor(Math.random() * lowercase.length)];
+ return lowercasevar [Math.floor(Math.random() * lowercase.length)];
 }
 
 function GetRandomUpper(){
-    return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+    return uppercasevar [Math.floor(Math.random() * lowercase.length)];
 }
 function GetRandomNumber(){
-    return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+    return numbervar [Math.floor(Math.random() * number.length)];
 }
 function GetRandomSymbol(){
-    return symbols [Math.floor(Math.random() * symbols.length)];
+    return symbolsvar [Math.floor(Math.random() * symbols.length)];
 }
 console.log (GetRandomSymbol());
 
