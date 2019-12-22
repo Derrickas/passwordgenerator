e7e119ea051d991574f322ab576248878d2de7c7
Derrickas <Derrickas@outlook.com>
2019-12-21 21:09:20 -0600

    finnaly finished js

--- Javascript.js
+++ Javascript.js
@@ -1,42 +1,113 @@
-var resultsEL =document.querySelector('symbol-box')
-var lengthEL =document.querySelector('length')
-var uppercasesEL =document.querySelector('uppercase-box')
-var lowercaseEL =document.querySelector('lowercase-box')
-var generateEL =document.querySelector('generate')
-var clipboardEL =document.querySelector('clipboard')
+// Dom elements
 
+const resultsEL = document.getElementById('password')
+const lengthEL = document.getElementById('length')
+const uppercaseEL = document.getElementById('uppercasebox')
+const lowercaseEL = document.getElementById('lowercasebox')
+const numbersEL = document.getElementById('numbersbox')
+const symbolsEL = document.getElementById('symbolbox')
+const generateEL = document.getElementById('generate')
+const clipboardEL = document.getElementById('copy to clipboard')
 
+// function variables
 
-const symbolsvar = '~!@#$%^&*()_+{}|:"<>?-=`'
-const lowercasevar = 'abcdefghijklmnopqrstuvwxyz'
-const uppercasevar = lowercase.toUpperCase()
-const numbervar = '0123456789'
+const symbols = '~!@#$%^&*()_+{}|:"<>?-=`'
+const lowercase = 'abcdefghijklmnopqrstuvwxyz'
+const uppercase = lowercase.toUpperCase()
+const number = '0123456789'
+
+// Generate functions
 
 const randomFunc = {
-    Upper: GetRandomUpper
-    lower: GetRandomLower
-    symbols: GetRandomSymbol
+    Upper: GetRandomUpper,
+    lower: GetRandomLower,
+    symbol: GetRandomSymbol,
     number: GetRandomUpper
 };
 
+generateEL.addEventListener('click', function () {
 
-function GetRandomLower(){
- return lowercasevar [Math.floor(Math.random() * lowercase.length)];
+    const length = +lengthEL.value;
+    const haslower = lowercaseEL.checked;
+    const hasupper = uppercaseEL.checked;
+    const hasnumber = numbersEL.checked;
+    const hassymbol = symbolsEL.checked;
+
+    resultsEL.innerText = generatepassword(haslower, hasupper, hassymbol, hasnumber, length);
+
+
+
+});
+
+function generatepassword(lower, upper, symbol, number, length) {
+
+    let generatedpassword = "";
+
+    const typescount = lower + upper + number + symbol;
+
+    console.log('typescount: ', typescount);
+
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter (item => Object.values(item)[0]);
+
+    console.log('typesArr: ', typesArr);
+
+    if (typescount === 0) {
+        return '';
+
+    }
+
+    for (let i=0; i<length; i+= typescount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+
+            console.log('funcName: ', funcName);
+
+            generatedpassword += randomFunc[funcName]();
+        });
+    }
+
+    const finalpassword = generatedpassword.slice(0, length);
+
+    return finalpassword;
 }
 
-function GetRandomUpper(){
-    return uppercasevar [Math.floor(Math.random() * lowercase.length)];
-}
-function GetRandomNumber(){
-    return numbervar [Math.floor(Math.random() * number.length)];
-}
-function GetRandomSymbol(){
-    return symbolsvar [Math.floor(Math.random() * symbols.length)];
-}
-console.log (GetRandomSymbol());
+    // // clipboard function
+    // const textarea = document.createElement('textarea')
+    // const password = resultsEL.innerText;
+
+    // if( !password)
+    // return;
+    // textarea.value = password;
+    // document.abody.appendchild(textarea);
+    // textarea.select();
+    // document.execCommand('copy')
+    // textarea.remove
+    // alert('password has been copied to clipboard')
 
 
 
-console.log (GetRandomNumber());
-console.log (GetRandomLower());
-console.log (GetRandomUpper());
+
+
+    // (
+    //     item => Object.values(item) [0]
+
+    // );
+
+
+    // }
+    function GetRandomLower() {
+        return lowercase[Math.floor(Math.random() * lowercase.length)];
+    }
+    function GetRandomUpper() {
+        return uppercase[Math.floor(Math.random() * lowercase.length)];
+    }
+    function GetRandomNumber() {
+        return number[Math.floor(Math.random() * number.length)];
+    }
+    function GetRandomSymbol() {
+        return symbols[Math.floor(Math.random() * symbols.length)];
+    }
+    // console.log(GetRandomSymbol());
+    // console.log(GetRandomNumber());
+    // console.log(GetRandomLower());
+    // console.log(GetRandomUpper());
